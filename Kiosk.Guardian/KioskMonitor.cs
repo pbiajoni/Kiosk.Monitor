bafa496ca71622edbb2f2f93ebc2306d60095ec7
pablo.biajoni <pbiajoni@hotmail.com>
2019-12-04 14:31:09 -0300

    Adicionar arquivos de projeto.

---
+++ Kiosk.Guardian/KioskMonitor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk.Guardian
+{
+    public class KioskMonitor
+    {
+        private string _proccessName;
+        private string _pathToServer;
+
+        [Category("MultiClubes")]
+        [DisplayName("Nome do Processo")]
+        [Description("Nome do processo do Multiclubes Kiosk")]
+        public string ProcessName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_proccessName))
+                {
+                    return "MultiClubes.Kiosk.UI";
+                }
+
+                return _proccessName;
+            }
+
+            set
+            {
+                _proccessName = value;
+            }
+        }
+
+        [Category("MultiClubes")]
+        [DisplayName("Caminho do Kiosk")]
+        [Description("Caminho do aplicativo Kiosk no servidor")]
+        public string Path
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_pathToServer))
+                {
+                    return @"\\servidor\multiclubessistemas$\Kiosk\MultiClubes.Kiosk.UI.application";
+                }
+
+                return _pathToServer;
+            }
+
+            set
+            {
+                _pathToServer = value;
+            }
+        }
+
+        [Category("Verificãção")]
+        [DisplayName("Intervalo")]
+        [Description("Intervalo em que será verificado se o Kiosk está em operação")]
+        public int Interval { get; set; }
+    }
+}
---
+++ Kiosk.Guardian/frmMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Kiosk.Guardian
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.process1 = new System.Diagnostics.Process();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid1.Location = new System.Drawing.Point(0, 0);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(433, 343);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 343);
+            this.Controls.Add(this.propertyGrid1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kiosk Multiclubes";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+    }
+}
+
